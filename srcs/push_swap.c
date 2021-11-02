4c253537027c935ffce3d2d002e55c42ba107283
Alienxbe <martin.ykman@gmail.com>
2021-11-02 16:49:02 +0100

    testing

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,59 +6,53 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 09:48:24 by mykman            #+#    #+#             */
-/*   Updated: 2021/11/01 23:14:01 by mykman           ###   ########.fr       */
+/*   Updated: 2021/11/02 11:03:50 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	print_stack(t_dlist *stack_a, t_dlist *stack_b)
+int	free_return(t_list *stack, int *content_list, int return_val)
 {
-	ft_printf("--------------------------------\n");
-	while (stack_a || stack_b)
-	{
-		if (stack_a)
-		{
-			ft_printf("%5s\t|", (char *)stack_a->content);
-			stack_a = stack_a->next;
-		}
-		else
-			ft_printf("     \t|");
-		if (stack_b)
-		{
-			ft_printf("\t%s\n", (char *)stack_b->content);
-			stack_b = stack_b->next;
-		}
-		else
-			ft_printf("\n");
-	}
-	ft_printf("%5s\t|\t%s\n", "-", "-");
-	ft_printf("%5s\t|\t%s\n", "A", "B");
+	free(content_list);
+	ft_lstclear(&stack, NULL);
+	return (return_val);
+}
+
+int	in_list(int *list, int index, int n)
+{
+	int i;
+
+	i = -1;
+	while (++i < --index)
+		if (list[i] == n)
+			return (1);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
+	t_list	*stack_a;
+	t_list	*new;
+	int		*content_list;
 	int		i;
-	t_dlist	*stack_a;
-	t_dlist	*stack_b;
-	t_dlist	*new;
 
 	if (argc < 2)
 		return (1);
 	stack_a = NULL;
-	stack_b = NULL;
+	content_list = (int *)ft_calloc(argc - 1, sizeof(int));
+	if (!content_list)
+		return (1);
 	i = 0;
 	while (++i < argc)
 	{
-		new = ft_dlstnew(argv[i]);
-		ft_dlstadd_back(&stack_a, new);
-		if (!new)
-			return (1);
+		content_list[i - 1] = ft_atoi(argv[i]);
+		new = ft_lstnew(content_list + i - 1);
+		if (!ft_isnumber(argv[i])
+			|| in_list(content_list, i - 1, content_list[i - 1])
+			|| !new)
+			return (free_return(stack_a, content_list, 1));
+		ft_lstadd_back(&stack_a, new);
 	}
-	print_stack(stack_a, stack_b);
-	ft_rx(&stack_a);
-	print_stack(stack_a, stack_b);
-	ft_dlstclear(&stack_a, NULL);
-	ft_dlstclear(&stack_b, NULL);
-	return (0);
+	return (free_return(stack_a, content_list, 0));
 }
