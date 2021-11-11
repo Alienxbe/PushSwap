1d7dba4b4b9659b3095986261e6d7fde88b7acc2
Alienxbe <martin.ykman@gmail.com>
2021-11-11 23:38:39 +0100

    Starting new branch

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 11:06:45 by mykman            #+#    #+#             */
-/*   Updated: 2021/11/11 23:21:39 by mykman           ###   ########.fr       */
+/*   Updated: 2021/11/11 23:38:07 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,10 @@
 ** MAIN FUNCTIONS
 */
 
-void	ft_sx(t_dlist **stack, char *text);
-void	ft_ss(t_dlist **stack_a, t_dlist **stack_b);
-void	ft_px(t_dlist **stack_from, t_dlist **stack_to, char *text);
-void	ft_rx(t_dlist **stack, char *text);
-void	ft_rr(t_dlist **stack_a, t_dlist **stack_b);
-void	ft_rrx(t_dlist **stack, char *text);
-void	ft_rrr(t_dlist **stack_a, t_dlist **stack_b);
-
-void	rotate_top(t_dlist **stack, t_dlist *elem);
-
-void	crap_sort(t_dlist **stack_a, t_dlist **stack_b);
-
 
 /*
 ** UTILS
 */
-
-t_dlist	*fill_stack(int argc, char **argv, int **content);
-
-int		is_sorted(t_dlist *stack);
-int		in_list(int *list, int index, int n);
-
-void	print_stack(t_dlist *stack_a, t_dlist *stack_b);
 
 
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,38 +6,13 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 09:48:24 by mykman            #+#    #+#             */
-/*   Updated: 2021/11/11 23:21:44 by mykman           ###   ########.fr       */
+/*   Updated: 2021/11/11 23:38:22 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	free_return(int value, t_dlist *stack, int *content)
-{
-	if (content)
-		free(content);
-	if (stack)
-		ft_dlstclear(&stack, NULL);
-	return (value);
-}
-
 int	main(int argc, char **argv)
 {
-	t_dlist	*stack_a;
-	t_dlist	*stack_b;
-	int		*content;
-
-	if (argc < 2)
-		return (1);
-	content = NULL;
-	stack_a = fill_stack(argc, argv, &content);
-	stack_b = NULL;
-	if (!stack_a || is_sorted(stack_a))
-		return (free_return(1, stack_a, content));
-	print_stack(stack_a, stack_b);
-	crap_sort(&stack_a, &stack_b);
-	print_stack(stack_a, stack_b);
-	ft_dlstclear(&stack_a, NULL);
-	ft_dlstclear(&stack_b, NULL);
 	return (0);
 }
